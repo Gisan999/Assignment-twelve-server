cc2b9fa03f42ed6909dce3a5bf7a50c8062a51cf
Gisan999 <ruheatheking3@gmail.com>
2023-11-26 04:34:09 +0600

    mongoose basic setup and error handling complete

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const applyMiddleware = require('./middleware/applyMiddleware');
+const connectDB = require('./db/connectDB');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+applyMiddleware(app);
+
+
+app.get('/health', (req, res) => {
+    res.send('final project is running')
+})
+
+app.all("*", (req, res, next) => {
+    const error = new Error(`the requested url is invalid: [${req.url}]`);
+    error.status = 404;
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    });
+})
+ 
+
+const main = async () => {
+    await connectDB();
+    app.listen(port, () => {
+        console.log(`final project server running on the port ${port}`);
+    })
+}
+
+main()
---
+++ src/db/connectDB.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const getConnectionString = () => {
+    let connectionURI;
+
+    if(process.env.NODE_ENV==="development"){
+        connectionURI = process.env.DATABASE_LOCAL
+        connectionURI = connectionURI.replace('<username>', process.env.DB_USER)
+        connectionURI = connectionURI.replace('<password>', process.env.DB_PASS)
+        
+    }else{
+        connectionURI 
+    }
+
+    return connectionURI
+}
+
+const connectDB = async () => {
+    const uri = getConnectionString();
+
+    await mongoose.connect(uri, {dbName:process.env.DB_NAME})
+}
+
+module.exports = connectDB
