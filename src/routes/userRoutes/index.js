3aac2e54747f788efd5922ccafb5c81146351614
Gisan999 <ruheatheking3@gmail.com>
2023-11-28 02:01:59 +0600

    token verification middleware complete

--- src/routes/userRoutes/index.js
+++ src/routes/userRoutes/index.js
@@ -1,10 +1,11 @@
 const adminCheck = require('../../api/controller/adminCheck');
 const getUser = require('../../api/controller/getUser');
 const postUser = require('../../api/controller/postUser');
+const verifyToken = require('../../middleware/verifyToken');
 
 const router = require('express').Router()
 
-router.get('/get/users', getUser)
+router.get('/get/users',  getUser)
 router.post('/set/users', postUser )
 router.get('/users/admin/:email', adminCheck )
 module.exports = router;
